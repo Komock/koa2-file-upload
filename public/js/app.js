bec5da22727651bd4dc19814bd34ba725f1a3ea5
Vadim <pv@petrovvadim.com>
2017-06-05 08:22:17 +0300

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,63 @@
+(function() {
+
+	var uploadURL = '/api/upload',
+		form = document.querySelector('.form'),
+		fileInput = document.querySelector('.form #fileInput');
+
+	fileInput.addEventListener('change', function(e) {
+		var file = e.target.files[0];
+		console.log(file);
+		var reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = function(event) {
+			// var blob = new Blob([event.target.result], { type: 'image/*' });
+			preview.src = event.target.result;
+		};
+	});
+
+	function upload(file) {
+		var xhr = new XMLHttpRequest();
+		// обработчик для закачки
+		xhr.upload.onprogress = function(event) {
+			progress.innerText = event.loaded + ' / ' + event.total;
+		}
+
+		// обработчики успеха и ошибки
+		// если status == 200, то это успех, иначе ошибка
+		xhr.onload = xhr.onerror = function() {
+			if (this.status == 200) {
+				console.log('success');
+			} else {
+				console.log('error ' + this.status);
+			}
+		};
+
+		xhr.open('POST', uploadURL, true);
+		xhr.send(file);
+	}
+
+	form.addEventListener('submit', function(e) {
+		e.preventDefault();
+		var file = fileInput.files[0];
+		upload(file);
+	});
+
+})();
+
+
+//====== As form data
+// var formData = new FormData(document.forms.fileForm);
+// // send
+// var xhr = new XMLHttpRequest();
+// xhr.open('POST', uploadURL);
+// xhr.send(formData);
+// xhr.addEventListener('load', function (e) {
+// 	if (this.readyState == 4) {
+// 		if (this.status == 200) {
+// 			// OK
+// 			console.log('Response', e.target.responseText);
+// 		} else {
+// 			console.error('XHR Error!')
+// 		}
+// 	}
+// })
---
+++ server.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+
+//====== Vendors
+const Koa = require('koa');
+const BodyParser = require('koa-bodyparser');
+const Body = require('koa-better-body')
+const KoaRouter = require('koa-router');
+const Static = require('koa-static');
+const Send = require('koa-send');
+const Multer = require('koa-multer');
+const AsyncBusboy = require('async-busboy');
+
+
+const App = new Koa();
+const Router = KoaRouter();
+// Multer
+const Storage = Multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'uploads/images/thumbs/');
+	},
+	filename: function (req, file, cb) {
+		console.log(file);
+		cb(null, file.originalname);
+	}
+});
+const Upload = Multer({
+	storage: Storage
+});
+
+
+//=== Body Parser 'koa-better-body'
+// App.use(Body());
+
+//=== Read static
+App.use(Static(__dirname + '/public/'));
+
+Router.get('/', async (ctx, next) => {
+	await Send(ctx, './public/index.html');
+});
+
+
+Router.post('/api/upload', async (ctx, next) => {
+
+	let dirIsEmpty = true;
+
+	await new Promise((res, rej)=> {
+		fs.readdir('uploads/images/thumbs/', function (err, items) {
+			if (items.length !== 0) dirIsEmpty = false;
+			if (err) console.error(err);
+			console.log(items, items.length);
+			console.log('dirIsEmpty', dirIsEmpty);
+			res();
+		});
+	});
+
+	if(!dirIsEmpty){
+		let msg = 'Dir already contain file!';
+		console.warn(msg);
+		ctx.body = msg;
+		return;
+	}
+	
+	await new Promise((res, rej)=> {
+		Upload.single('fileInput')(ctx, (...args) => {
+			console.log(args);
+			res();
+		});
+	});
+
+
+	ctx.body = 'Loaded!';
+});
+
+
+App.use(Router.routes()).use(Router.allowedMethods());
+
+
+App.listen(3030);
